7eeabe4a98a61e98b811f47fbb2b39391dc371b4
Manjitsingh <manjit7193@gmail.com>
2014-04-15 17:05:55 +0530

    add new file

--- sites/all/themes/spacious/template.php
+++ sites/all/themes/spacious/template.php
@@ -54,3 +54,36 @@ function spacious_links__system_secondary_menu(&$variables) {
     }
     return $output;
 }
+
+/**
+ *  Status messages alter
+ */
+
+function spacious_status_messages($variables) {
+  $display = $variables['display'];
+  $output = '';
+
+  $status_heading = array(
+    'status' => t('Status message'),
+    'error' => t('Error message'),
+    'warning' => t('Warning message'),
+  );
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    $output .= "<div class=\"messages $type\">\n";
+    if (!empty($status_heading[$type])) {
+      $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+    }
+    if (count($messages) > 0) {
+      $output .= " <ul>\n";
+      foreach ($messages as $message) {
+        $output .= '  <li>' . $message . "</li>\n";
+      }
+      $output .= " </ul>\n";
+    }
+    else {
+      $output .= $messages[0];
+    }
+    $output .= "</div>\n";
+  }
+  return $output;
+}
