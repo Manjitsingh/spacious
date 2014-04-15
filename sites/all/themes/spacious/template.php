c554100c8c97c002a8e474ead34459357e2ea07d
Manjitsingh <manjit7193@gmail.com>
2014-04-15 16:58:03 +0530

    add new file

--- sites/all/themes/spacious/template.php
+++ sites/all/themes/spacious/template.php
@@ -5,6 +5,7 @@
  *
  * Print breadcrumbs as a list, with separators.
  */
+
 function spacious_breadcrumb($variables) {
 	global $base_url;
 	$breadcrumb = $variables['breadcrumb'];
@@ -23,4 +24,33 @@ function spacious_breadcrumb($variables) {
 		$breadcrumbs .= '</ol>';
 		return $breadcrumbs;
 	}
+}
+
+/**
+ *  Menu tree alter
+ */
+
+function spacious_menu_tree($variables) {
+  return '<ul class="margin0 menu padding0">' . $variables['tree'] . '</ul>';
+}
+
+/**
+ *  Secondary menu alter
+ */
+
+function spacious_links__system_secondary_menu(&$variables) {
+    $output = '';
+    $extra_classes = array(
+        'user' => 'secnav_account',
+        'user/logout' => 'secnav_logout',
+    );
+
+    foreach ($variables['links'] as $item => $link) {
+        $classes = array($item);
+        if (isset($extra_classes[$link['href']])) {
+            $classes[] = $extra_classes[$link['href']];
+        }
+        $output .= sprintf('<li class="%s">%s</li>', implode(' ', $classes), l($link['title'], $link['href']));
+    }
+    return $output;
 }
